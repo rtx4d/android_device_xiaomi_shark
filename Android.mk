017bebbc6c805c65d5d809a727a6a9e401484764
Bruno Martins <bgcngm@gmail.com>
2025-04-07 01:07:11 +0700

    shark: Drop all Android makefiles inclusion within the tree
    Change-Id: I33a163d7243fe1203538d6e37a907acd3976f1d4

--- Android.mk
+++ Android.mk
@@ -1,13 +1,11 @@
 #
-# Copyright (C) 2018-2020, 2024 The LineageOS Project
+# Copyright (C) 2018-2020, 2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter shark,$(TARGET_DEVICE)),)
-include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
